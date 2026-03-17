fff4c29e385207a53a3b2b9a982866dd8515cd2a
kevin molina <kevinxdantonio2007@gmail.com>
2026-03-17 06:19:09 -0600

    Preparando deploy en Vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 ﻿import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://fnoppzknyvluzpaomuzv.supabase.co"
-const supabaseKey = "sb_publishable_2S21u7vXqRBqZQf4sVwZ1g_Eym7fNCt"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
